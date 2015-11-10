d5dc43f46eba8824d1635b773628b23f64c7b679
Lu(Rex) Yang <Rex@Lus-MacBook-Pro.local>
2015-11-10 14:34:22 -0500

    1

--- receiver.cpp
+++ receiver.cpp
@@ -217,7 +217,6 @@ int main(int argc, char **argv)
     write(sockfd,s_add.c_str() , s_add.size() );
     //local = string(addtmp,strlen(addtmp));
     local = "127.0.0.1";
-    fwrite(tmp.c_str(), 1, tmp.size(), logfs);
     //////////
     std::vector<out_order_buffer_unit> out_order_buf;
     
